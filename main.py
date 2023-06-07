707fcf87ef18b7d3c9b126aaed51e20c49750bbb
Saul Romo <dsromo@proton.me>
2023-06-07 00:16:47 -0500

    fix 3

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from langchain.chains import ConversationChain
 from langchain.memory import ConversationEntityMemory
 
 # load environment variables
-TOKEN = os.environ("DISCORD_BOT_TOKEN")
+TOKEN = os.environ["DISCORD_BOT_TOKEN"]
 
 # set up Discord bot intents
 intents = discord.Intents.all()
