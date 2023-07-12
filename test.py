0cf8552592f2212c4ee2abfac21653116e973d9e
Saul Romo <dsromo@proton.me>
2023-07-12 18:25:36 -0500

    update

---
+++ test.py
@@ -0,0 +1,10 @@
+class Item:
+    pass
+item1 = Item()
+item1.name = "Phone"
+item1.price = 100
+item1.quantity = 5
+print(item1) # item
+print(item1.name) # str
+print(item1.price) # int
+print(item1.quantity) # int
